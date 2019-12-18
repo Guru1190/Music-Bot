241f88b205bf74f3dde367089edf38593216ad97
Guru1190 <58129762+Guru1190@users.noreply.github.com>
2019-12-19 05:27:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NjU2NjQ1MDIxMjk4MDY1NDE4.Xfq8vQ.OUbTn7_3DNFWS9O1IgBN-fbmxY8';
 
 exports.PREFIX = 'm!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyDcqjAM_bzwbseSsMk_YZk4qENcXJORCCE';
